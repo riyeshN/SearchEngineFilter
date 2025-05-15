90dd028b09cc0dcb822ebc4bb9751433431f0123
riyeshN <nathriyesh@gmail.com>
2025-05-14 21:31:46 -0400

    UI added

--- searchEngineFrontEnd/src/components/GetUrls.tsx
+++ searchEngineFrontEnd/src/components/GetUrls.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Grid, TextField, Box, Stack } from "@mui/material";
+import { Button, Grid, TextField, Box, Stack, Typography } from "@mui/material";
 import api from "../api/api";
 import ShowSearches from "./organization/ShowSearches";
 
@@ -7,6 +7,7 @@ const GetUrls = () => {
 	const [searchVal, setSearchVal] = useState("");
 	const [loading, setLoading] = useState(false);
 	const [listOfUrls, setListOfUrls] = useState([]);
+	const [listOfStats, setListOfStats] = useState({});
 	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchVal(e.target.value);
 	};
@@ -25,6 +26,26 @@ const GetUrls = () => {
 			);
 			setListOfUrls(response.data.data);
 			console.log("urls", response.data.data);
+
+			const response2 = await api.get("searchFilter/get_list_of_ads_none_ads", {
+				params: {
+					keyword: searchVal,
+				},
+			});
+
+			const searchEngineStats = {};
+			response2.data.data.forEach((element) => {
+				const engine = element.searchEngineName_id;
+				const adPromo = element.ad_promo;
+				const count = element["COUNT(*)"];
+
+				if (!searchEngineStats[engine]) {
+					searchEngineStats[engine] = {};
+				}
+				searchEngineStats[engine][adPromo] = count;
+			});
+			setListOfStats(searchEngineStats);
+			console.log("urls", searchEngineStats);
 		} catch (error) {
 			console.log("ERROR", error);
 		} finally {
@@ -76,7 +97,21 @@ const GetUrls = () => {
 						{loading ? "Loading" : "Search"}
 					</Button>
 				</Grid>
-
+				<Grid container spacing={2}>
+					{Object.entries(
+						listOfStats as Record<string, Record<string, number>>
+					).map(([engine, stats]) => (
+						<Grid size={6} key={engine}>
+							<Typography>
+								{`${engine} - Ads: ${stats["true"] ?? 0} - Not Ads: ${
+									stats["false"] ?? 0
+								} - Percent Ads ${(
+									(stats["true"] ?? 0) / (stats["false"] ?? 0)
+								).toFixed(2)}`}
+							</Typography>
+						</Grid>
+					))}
+				</Grid>
 				<Grid size={12}>
 					<Box sx={{ width: "100%", marginTop: 3 }}>{renderTable()}</Box>
 				</Grid>
